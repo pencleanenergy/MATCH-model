56b7593a2001072b471a501d0cdc2af0ed05548e
grgmiller <grmiller@ucdavis.edu>
2021-05-24 12:06:45 -0700

    2021.05.24 Fix reporting

--- switch_model/reporting/generate_report.py
+++ switch_model/reporting/generate_report.py
@@ -11,6 +11,9 @@ def post_solve(instance, outdir, inputs_dir):
     os.system(f'jupyter nbconvert --to html --no-input --no-prompt {inputs_dir}/summary_report.ipynb --output-dir {outdir} --output summary_report_{s}')
     os.system(f'jupyter nbconvert --clear-output --inplace {inputs_dir}/summary_report.ipynb')
 
+    # NOTE: 5/24/2021: Temporarily disabling the public reporting functionality until the main summary report is finalized
+    """
     os.system(f'jupyter nbconvert --ExecutePreprocessor.kernel_name="python3" --to notebook --execute --inplace {inputs_dir}/summary_report_public.ipynb')
     os.system(f'jupyter nbconvert --to html --no-input --no-prompt {inputs_dir}/summary_report_public.ipynb --output-dir {outdir} --output summary_report_{s}_public')
     os.system(f'jupyter nbconvert --clear-output --inplace {inputs_dir}/summary_report_public.ipynb')
+    """
